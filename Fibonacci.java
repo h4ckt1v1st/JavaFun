3a069a793f061d5ff7781ccaf5804206b4b37cbf
Abhishek Singh <abhishek.viki@gmail.com>
2022-02-05 06:25:32 +0530

    Added fibonacci using memoization.

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+public class Fibonacci {
+
+    private static long[] fibonacciCache;
+
+    public static void main(String[] args) {
+        // For long data type, the limit is 92nd Fibonacci number.
+        int n = 92;
+        fibonacciCache = new long[n + 1];
+
+        System.out.println(fibonacci(n));
+    }
+
+    private static long fibonacci(int n) {
+        if (n  <= 1) {
+            return n;
+        }
+
+        if (fibonacciCache[n] != 0) {
+            return fibonacciCache[n];
+        }
+
+        long nthFibonacciNumber = (fibonacci(n - 1) + fibonacci(n - 2));
+        fibonacciCache[n] = nthFibonacciNumber;
+
+        return nthFibonacciNumber;
+    }
+}
